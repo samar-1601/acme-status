0fd31b76c37449ee1a97333bf149a536fe346f08
samar-1601 <samar.pp.singh@gmail.com>
2022-05-30 15:35:35 +0530

    css modified | Back home functionality styled

--- pages/tic-tac-toe/samar.js
+++ pages/tic-tac-toe/samar.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import TicTacToe from "../../components/samar-tic-tac-toe/TicTacToe";
+import styles from '../../styles/Home.module.css'
+
 
 export default function TicTacToeGame() {
   return (
-    <>
+    <div className={styles.main} >
       <TicTacToe />
-      <Link href="/" > Go back Home</Link>
-    </>
+      <Link href="/">
+        <button className="backHome">Go back Home</button>
+      </Link>
+    </div >
   );
 }
