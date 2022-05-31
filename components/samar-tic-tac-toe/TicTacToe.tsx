09d7119fa45b4ea715da2dff549dcdbbc97dcd7c
samar-1601 <samar.pp.singh@gmail.com>
2022-05-31 15:04:49 +0530

    updated some parts following the clean code guidelines

--- components/samar-tic-tac-toe/TicTacToe.tsx
+++ components/samar-tic-tac-toe/TicTacToe.tsx
@@ -3,21 +3,29 @@ import { useState } from "react";
 import { CalculateWinner } from "./calculateWinner";
 import styles from "./style.module.css";
 
-export let winner:string = ""; // variable to store the winner 'X' or 'O'
-let winning_cells:number[] = [];
-let history = [{
-  squares: Array(9).fill(null),
-  row: "",
-  col: ""
-}];
-let moves;
+export let winner: string = ""; // variable to store the winner 'X' or 'O'
+let winning_cells: number[] = []; // list of numbers storing the combination for the winning player
 
- // contains the history in terms of array of squre values at each step
-let stepNumber:number = 0; // counter for storing the steps taken in game
-let status:string; // stores status of the game
+interface historyInterface {
+  squares: string[];
+  row: string;
+  col: string;
+}
+
+let history:historyInterface[] = [
+  {
+    squares: Array(9).fill(null),
+    row: "",
+    col: "",
+  },
+];
+
+// contains the history in terms of array of squre values at each step
+let stepNumber: number = 0; // counter for storing the steps taken in game
+let status: string; // stores status of the game
 
 // temporary array to store the current state or the current value held by each of the 9 squares
-let squares:string[] = Array(9).fill("");
+let squares: string[] = Array(9).fill("");
 
 // fill the history with empty values before the game starts
 
@@ -37,45 +45,43 @@ function TicTacToe() {
   };
 
   // Function to Toggle the player and update the related variables
-  const togglePlayer = (index:number) => {
+  const togglePlayer = (index: number) => {
     updateHistory();
 
     // update the latest squares array with the box value clicked by the player
     squares[index] = player;
 
     // update the history list
-    history = history.concat(
-      [{
-        squares : squares,
-        row : String(Math.floor(index / 3)),
-        col: String(index % 3,)
-      }]
-    );
+    history = history.concat([
+      {
+        squares: squares,
+        row: String(Math.floor(index / 3)),
+        col: String(index % 3),
+      },
+    ]);
 
     // No. of steps taken in game is updated
     stepNumber = history.length;
 
     // Check for a winner
     [winner, winning_cells] = CalculateWinner(squares);
-    
+
     // update the square-list for the latest grid
     setSquareValues(squares);
-    
+
     // update the player who's turn has come according to the game stage
-    if (stepNumber%2!==0) setPlayer("X");
+    if (stepNumber % 2 !== 0) setPlayer("X");
     else setPlayer("O");
   };
 
   // Function to Jump to a new state of the Game based on the history
   // level clicked
-  const jumpTo = (step:number) => {
+  const jumpTo = (step: number) => {
     console.log("step : " + step);
     console.log("stepNumber : " + stepNumber);
-    
-    if(step===history.length-1)
-      return;
 
-    
+    if (step === history.length - 1) return;
+
     stepNumber = step;
     // update the history to execute this jump
     updateHistory();
@@ -85,7 +91,7 @@ function TicTacToe() {
     winning_cells = [];
     setSquareValues(squares);
 
-    if (stepNumber%2===0) setPlayer("X");
+    if (stepNumber % 2 === 0) setPlayer("X");
     else setPlayer("O");
 
     console.log("player : " + player);
@@ -95,31 +101,29 @@ function TicTacToe() {
   // Update the moves list according to the history
   const moves = history.map((step, move) => {
     // decide the text for the status
-    const desc = move ? "Go to move #" + move + `(${step.row}, ${step.col})` : "Go to game start";
+    const desc = move
+      ? "Go to move #" + move + `(${step.row}, ${step.col})`
+      : "Go to game start";
     return (
       <li key={move}>
-        <button className={styles.historyButton} onClick={()=>jumpTo(move)}>
+        <button className={styles.historyButton} onClick={() => jumpTo(move)}>
           {desc}
         </button>
       </li>
     );
   });
 
-  
   // changing the order of moves based on ascending or descending
-  if(!ascending) moves.reverse();
+  if (!ascending) moves.reverse();
   // toggling the ascending/ descending feature
-  const toggleAscending = ()=>{
+  const toggleAscending = () => {
     setAscending(!ascending);
   };
-  
-  
+
   // Update the status of game
-  if(!winner && moves.length===10)
-  {
+  if (!winner && moves.length === 10) {
     status = "DRAW";
-  }
-  else if (winner) {
+  } else if (winner) {
     status = "Winner: " + winner;
   } else {
     status = "Next player: " + player;
@@ -136,7 +140,9 @@ function TicTacToe() {
       </div>
       <div className={styles.gameInfo}>
         <div>{status}</div>
-        <button onClick={()=> toggleAscending()}>{ascending?"Ascending":"Descending"}</button>
+        <button onClick={() => toggleAscending()}>
+          {ascending ? "Ascending" : "Descending"}
+        </button>
         <ol>{moves}</ol>
       </div>
     </div>
--- components/samar-tic-tac-toe/square.tsx
+++ components/samar-tic-tac-toe/square.tsx
@@ -2,26 +2,34 @@ import styles from "./style.module.css";
 import { winner } from "./TicTacToe";
 import React from "react";
 
-interface Props{
-  index: number, 
-  player: string, 
-  toggleButton: (idx:number)=> any, 
-  winners: string
+interface Props {
+  squareIndex: number;
+  player: string;
+  toggleButton: (idx: number) => any;
+  winners: string;
 }
 
-export const Square : React.FC<Props> = ({index, player, toggleButton, winners}) => {
-    const update = () => {
+export const Square: React.FC<Props> = ({
+  squareIndex,
+  player,
+  toggleButton,
+  winners,
+}) => {
+  // function for callling the parent to let it know that this square has been clicked
+  // and update the board status
+  const update = (): void => {
     // if winner has been decided or this box was already checked, don't mark it again
-    if(!player &&  !winner)
-    {
-      console.log(index);
-      toggleButton(index);
+    if (!player && !winner) {
+      toggleButton(squareIndex);
     }
   };
-  const square  = "square";
+  const square = "square";
   return (
-    <button id = {String(index)} className={`${styles.square} ${winners?styles.winner:""}`} onClick = {()=>update()}>
+    <button
+      className={`${styles.square} ${winners ? styles.winner : ""}`}
+      onClick={() => update()}
+    >
       {player}
     </button>
   );
-}
+};
