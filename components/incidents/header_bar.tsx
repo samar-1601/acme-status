84726c37df0110d39d4483b2ddbf3991eb1715e3
samar-1601 <samar.pp.singh@gmail.com>
2022-06-02 03:54:21 +0530

    Ts added to HeaederBar

--- components/incidents/header_bar.tsx
+++ components/incidents/header_bar.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css"
 
-export const HeaderBar = ()=>{
+export const HeaderBar:React.FC = ()=>{
     return(
         <div className={styles.headerBar}>
             <h1>Incidents</h1>
