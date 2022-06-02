813f4b603abfb0855ce2936f0bb846b98f741c9f
samar-1601 <samar.pp.singh@gmail.com>
2022-06-02 11:30:48 +0530

    added components below the list-items in the incidents/open page

--- components/incidents/header_tabs.tsx
+++ components/incidents/header_tabs.tsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Tabs, Tab } from "baseui/tabs-motion";
 import { OpenListView } from "./open_list_view";
 
-export const HeaderTabs:React.FC = () => {
+export const HeaderTabs: React.FC = () => {
   const [activeKey, setActiveKey] = useState<number>(0);
+
+  // getting the page ids for the next call
+  const [dataList, setData] = useState([]);
+  const URL = "https://api.statuspage.io/v1/pages";
+  const getData = () => {
+    fetch(URL, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `OAuth ${process.env.NEXT_PUBLIC_AUTH_TOKEN ?? ""}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((myJson) => setData(myJson));
+  };
+
+  useEffect(() => {
+    getData();    
+  }, []);
+
+  const idList:string[] = dataList.map((data) => data["id"]);
+
   return (
     <Tabs
       activeKey={activeKey}
@@ -14,20 +35,25 @@ export const HeaderTabs:React.FC = () => {
       overrides={{
         TabBorder: {
           style: () => ({
-            backgroundColor: '#DCDCDC'
-          })
+            backgroundColor: "#DCDCDC",
+          }),
         },
         TabHighlight: {
           style: () => ({
-            backgroundColor: 'blue'
-          })
+            backgroundColor: "blue",
+          }),
         },
-
       }}
     >
-      <Tab title="Open" ><OpenListView/></Tab>
-      <Tab title="Incidents" disabled>Incidents</Tab>
-      <Tab title="Maintainances" disabled>Maintainances</Tab>
+      <Tab title="Open">
+        <OpenListView idList={idList} />
+      </Tab>
+      <Tab title="Incidents" disabled>
+        Incidents
+      </Tab>
+      <Tab title="Maintainances" disabled>
+        Maintainances
+      </Tab>
     </Tabs>
   );
-}
+};
--- components/incidents/open_list_view.tsx
+++ components/incidents/open_list_view.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-export const OpenListView:React.FC = () => {
-  const [dataList, setData] = useState([]);
-  const URL = "https://api.statuspage.io/v1/pages/7dwwybj29fy8/incidents";
-  const getData = () => {
+interface Props {
+  idList: string[];
+}
+
+export const OpenListView: React.FC<Props> = ({ idList }) => {
+  const [dataList, setData] = useState<any[]>([]);
+
+  const getData = (pageId: string) => {
+    const URL = `https://api.statuspage.io/v1/pages/${pageId}/incidents`;
     fetch(URL, {
       headers: {
         "Content-Type": "application/json",
@@ -12,25 +17,26 @@ export const OpenListView:React.FC = () => {
       },
     })
       .then((response) => response.json())
-      .then((myJson) => setData(myJson));
+      .then((dataItem) => setData(dataItem));
   };
 
   useEffect(() => {
-    getData();
-  }, []);
-  console.log(dataList);
+    idList.forEach(getData);
+  }, [idList]);
 
   const classValue = (status: string) => {
     let style: string = styles.itemStatus;
     if (status === "investigating") {
       style = `${style} ${styles.bgBlue}`;
     }
+    if (status === "resolved") {
+      style = `${style} ${styles.bgGreen}`;
+    }
     return style;
   };
 
   const formatDate = (date: string | Date) => {
     date = new Date(date);
-    console.log(date);
     const timeElapsed = Date.now() - date.getTime();
 
     let seconds = timeElapsed / 1000;
@@ -43,23 +49,32 @@ export const OpenListView:React.FC = () => {
     return timeStatus;
   };
 
-  return (
-    <>
-      {dataList.map((data, id) => {
-        return (
-          <div key={id} className={styles.listItem}>
-            <div className={styles.listDetails}>
-              <span className={styles.itemName}>{data["name"]}</span>
-              <span className={classValue(data["status"])}>
-                {data["status"]}
-              </span>
-              <span className={styles.itemDate}>
-                {formatDate(data["created_at"])}
-              </span>
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
+  const getComponents = (data: any) => {
+    let componentsList: JSX.Element[] = [];
+    data["components"].forEach((component: any) => {
+      componentsList.push(
+        <span className={styles.componentItem}>
+          <span>{component["name"]}</span>
+        </span>
+      );
+    });
+    return componentsList;
+  };
+
+  let listItems: JSX.Element[] = dataList.map((data, id) => {
+    return (
+      <div key={id} className={styles.listItem}>
+        <div className={styles.listDetails}>
+          <span className={styles.itemName}>{data["name"]}</span>
+          <span className={classValue(data["status"])}>{data["status"]}</span>
+          <span className={styles.itemDate}>
+            {formatDate(data["created_at"])}
+          </span>
+          <span className={styles.component}>{getComponents(data)}</span>
+        </div>
+      </div>
+    );
+  });
+  console.log(dataList);
+  return <>{listItems}</>;
 };
