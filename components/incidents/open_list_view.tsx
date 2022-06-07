67b826ae67c2baafff32321173929a9e39f4f6c2
samar-1601 <samar.pp.singh@gmail.com>
2022-06-07 14:05:16 +0530

    react-virtualisation infinite loader added

--- components/incidents/open_list_view.tsx
+++ components/incidents/open_list_view.tsx
@@ -6,6 +6,15 @@ import { PageType } from "./incident_list_view";
 import { StyledSpinnerNext } from "baseui/spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
 
+import {
+  InfiniteLoader,
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
+import "react-virtualized/styles.css";
+
 /**
  * Status' classValue
  * @param { string } status Status's name obtained in API response
@@ -55,6 +64,12 @@ let prevDataLength: number = 0;
 export const OpenListView: React.FC<Props> = ({ pageType }) => {
   const [pageNumber, setPageNumber] = useState<number>(1); // stores the page number for infinite scrolling and data-fetching
   const [pageLoaded, setPageLoaded] = useState<boolean>(false); // boolean value determining the status of API resquest (completed/not completed)
+  const cache = useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 50,
+    })
+  );
 
   /**
    * API response
@@ -113,6 +128,7 @@ export const OpenListView: React.FC<Props> = ({ pageType }) => {
    * The functions triggers more data to load by changing the pageNumber's state and hence triggering the above useEffects
    */
   const fetchMoreData = () => {
+    console.log("fetchMore Called, pageNo : ", pageNumber);
     setPageNumber((p) => p + 1);
   };
 
@@ -208,27 +224,56 @@ export const OpenListView: React.FC<Props> = ({ pageType }) => {
    */
   const displayItemList = renderListData(dataList);
 
+  const loadMoreRows = (param: any) =>{  
+    const startIndex = param.startIndex;
+    const stopIndex = param.stopIndex;
+    
+    const dataLoaded = [];    
+    for (let i = startIndex; i < stopIndex; i++) {
+        dataLoaded[i] = displayItemList[i];
+    }
+    fetchMoreData();
+    return Promise.resolve(dataLoaded);
+}
+
   return pageLoaded ? (
-    <InfiniteScroll
-      dataLength={displayItemList.length}
-      next={fetchMoreData}
-      hasMore={hasMore}
-      loader={
-        <div className={styles.spinner}>
-          <StyledSpinnerNext />
-        </div>
-      }
-      className={styles.itemList}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>You've reached the end of page</b>
-        </p>
-      }
+    <InfiniteLoader
+      isRowLoaded={({index}) => !hasMore || index < displayItemList.length}
+      loadMoreRows={loadMoreRows}
+      rowCount={displayItemList.length+1}
     >
-      <div>
-        {displayItemList.length > 0 ? displayItemList : "No items found!!"}
-      </div>
-    </InfiniteScroll>
+      {({ onRowsRendered, registerChild }) => (
+        <div style={{ width: "100%", height: "100vh" }}>
+          <AutoSizer>
+            {({ width, height }) => (
+              <List
+                width={width}
+                height={height}
+                onRowsRendered={onRowsRendered}
+                ref={registerChild}
+                rowHeight={cache.current.rowHeight}
+                deferredMeasurementCache={cache.current}
+                rowCount={displayItemList.length}
+                rowRenderer={({ key, index, style, parent }) => {
+                  const element = displayItemList[index];
+                  return (
+                    <CellMeasurer
+                      key={key}
+                      cache={cache.current}
+                      parent={parent}
+                      columnIndex={0}
+                      rowIndex={index}
+                    >
+                      <div style={style}>{element}</div>
+                    </CellMeasurer>
+                  );
+                }}
+              ></List>
+            )}
+          </AutoSizer>
+        </div>
+      )}
+    </InfiniteLoader>
   ) : (
     <div className={styles.spinner}>
       <StyledSpinnerNext />
