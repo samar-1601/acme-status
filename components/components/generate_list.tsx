5b63612e7126872859530c199d2a8d140ec4882a
yash-burnwal <byash.iitk@gmail.com>
2022-06-09 16:40:03 +0530

    basic structuring

--- components/components/generate_list.tsx
+++ components/components/generate_list.tsx
@@ -1,18 +1,41 @@
 import { NEXT_PUBLIC_AUTH_TOKEN } from "../../constants";
 import styles from "./styles.module.css";
 
-export const generateList = function (dataList: any) {
+const Item = function (props: any) {
+  const URL = `https://api.statuspage.io/v1/pages/${props.comp.page_id}/components/${props.comp.id}/uptime`;
+  fetch(URL, {
+    headers : { 
+      "Content-Type": "application/json",
+      Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
+    }
+  })
+  .then(response => response.json())
+  .then(response => {
+    props.comp.description = "uptime data unavailable!";
+    if(!response.error) {
+      const date1 = new Date(response.range_end);
+      const date2 = new Date(response.range_start);
+      let days = Math.ceil(Math.abs(date1.valueOf()-date2.valueOf())/(1000*60*60*24));
+      props.comp.description = String(response.uptime_percentage)+"% uptime in the past "+String(days)+" days";
+    }
+  });
+  console.log(props.comp)
+  return (
+    <div key={props.comp.id} className={styles.listItem}>
+      <span className={styles.itemName}>{props.comp.name}</span><br/>
+      <span className={styles.itemDescription}>{props.comp.description}</span>
+    </div>)
+} 
+
+export const GenerateList = function (props: any) {
+  const dataList = props.dataList;
   console.log("here",dataList)
 
   let listItems = [];
 
   for (let i=0; i<dataList.length; i++) {
     console.log(i, dataList[i])
-    listItems[i] = (
-      <div key={dataList[i]["id"]} className={styles.listItem}>
-        <span className={styles.itemName}>{dataList[i]["name"]}</span><br/>
-        <span className={styles.itemDescription}>{dataList[i]["description"]}</span>
-      </div>)
+    listItems[i] = <Item comp={dataList[i]} />
   }
 
   return <div className={styles.itemList}>{listItems}</div>;
--- components/components/list_view.tsx
+++ components/components/list_view.tsx
@@ -1,4 +1,4 @@
-import { generateList } from "./generate_list";
+import { GenerateList } from "./generate_list";
 import { useEffect, useState } from "react";
 import { NEXT_PUBLIC_AUTH_TOKEN } from "../../constants";
 
@@ -6,34 +6,11 @@ let stat = "uptime data unavailable!";
 
 const ListView = function () {
   const [dataList, setDataList] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   let idList=[];
   let pages: any;
 
-  const getUpTimeStatus = async (pageId: string, componentId: string) => {
-    const URL = `https://api.statuspage.io/v1/pages/${pageId}/components/${componentId}/uptime`;
-
-    fetch(URL, {
-      headers : {
-        "Content-Type": "application/json",
-        Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
-      }
-    })
-    .then(response => response.json())
-    .then(response => {
-      stat = "uptime data unavailable!";
-      if(!response.error)
-      {
-        const date1 = new Date(response.range_end);
-        const date2 = new Date(response.range_start);
-        let days = Math.ceil(Math.abs(date1.valueOf()-date2.valueOf())/(1000*60*60*24));
-        stat = String(response.uptime_percentage)+"% uptime in the past "+String(days)+" days";
-      }
-      console.log(stat)
-      return stat;
-    })
-  }
-
-  const getComponentsPage = async (pageId : string) => {
+  const getComponentsFromPage = async (pageId : string) => {
     const URL = `https://api.statuspage.io/v1/pages/${pageId}/components`;
     const response = await fetch(URL, {
       headers : {
@@ -60,41 +37,28 @@ const ListView = function () {
 
   const getComponents = async () => {
     pages = await getPageID();
+    let tmp = [];
     pages.forEach(async (page: string) => {
-      let pageComponents = await getComponentsPage(page);
-      let tmp = [];
-      let i=0;
-      pageComponents.forEach(comp => {
-        const URL = `https://api.statuspage.io/v1/pages/${comp.page_id}/components/${comp.id}/uptime`;
-
-        fetch(URL, {
-          headers : {
-            "Content-Type": "application/json",
-            Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
-          }
-        })
-        .then(response => response.json())
-        .then(response => {
-          comp.description = "uptime data unavailable!";
-          if(!response.error) {
-            const date1 = new Date(response.range_end);
-            const date2 = new Date(response.range_start);
-            let days = Math.ceil(Math.abs(date1.valueOf()-date2.valueOf())/(1000*60*60*24));
-            comp.description = String(response.uptime_percentage)+"% uptime in the past "+String(days)+" days";
-          }
-        });
+      let pageComponents = await getComponentsFromPage(page);
+      let i: number=0;
+      pageComponents.forEach((comp: any) => {
         tmp[i++]=comp;
       })
-      setDataList(tmp);
     });
+    setDataList(tmp);
   }
 
   useEffect(() => {
-    getComponents();
+    if(dataList.length>0) setLoaded(true);
+    else {
+      getComponents();
+    }
   },[]);
 
-  console.log(dataList)
-  return <>{generateList(dataList)}</>;
+
+  if(loaded)  return <GenerateList dataList={dataList} />;
+  else return <>No items</>;
+
 };
 
 export default ListView;
