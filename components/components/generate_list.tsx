b9b70e724a9e574ceb3b5d587bc6b0f9fecdf52e
yash-burnwal <byash.iitk@gmail.com>
2022-06-09 18:20:27 +0530

    fixed erros in fetch :)

--- components/components/generate_list.tsx
+++ components/components/generate_list.tsx
@@ -1,41 +1,45 @@
+import { useEffect, useState } from "react";
 import { NEXT_PUBLIC_AUTH_TOKEN } from "../../constants";
 import styles from "./styles.module.css";
 
 const Item = function (props: any) {
-  const URL = `https://api.statuspage.io/v1/pages/${props.comp.page_id}/components/${props.comp.id}/uptime`;
-  fetch(URL, {
-    headers : { 
-      "Content-Type": "application/json",
-      Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
-    }
-  })
-  .then(response => response.json())
-  .then(response => {
-    props.comp.description = "uptime data unavailable!";
-    if(!response.error) {
-      const date1 = new Date(response.range_end);
-      const date2 = new Date(response.range_start);
+  const [msg, setMsg] = useState("uptime data unavailable!");
+
+  const getUptime = async () => {
+    const URL = `https://api.statuspage.io/v1/pages/${props.comp.page_id}/components/${props.comp.id}/uptime`;
+    const response = await fetch(URL, {
+      headers : { 
+        "Content-Type": "application/json",
+        Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
+      }
+    })
+    let xjson = await response.json();
+    if(!xjson.error) {
+      const date1 = new Date(xjson.range_end);
+      const date2 = new Date(xjson.range_start);
       let days = Math.ceil(Math.abs(date1.valueOf()-date2.valueOf())/(1000*60*60*24));
-      props.comp.description = String(response.uptime_percentage)+"% uptime in the past "+String(days)+" days";
+      setMsg(String(xjson.uptime_percentage)+"% uptime in the past "+String(days)+" days");
     }
-  });
-  console.log(props.comp)
+  }
+
+  useEffect(()=>{
+    getUptime();
+  },[]);
+
   return (
-    <div key={props.comp.id} className={styles.listItem}>
+    <div className={styles.listItem}>
       <span className={styles.itemName}>{props.comp.name}</span><br/>
-      <span className={styles.itemDescription}>{props.comp.description}</span>
+      <span className={styles.itemDescription}>{msg}</span>
+      <span>{props.comp.status}</span>
     </div>)
 } 
 
 export const GenerateList = function (props: any) {
-  const dataList = props.dataList;
-  console.log("here",dataList)
+  let dataList = props.dataList;
 
   let listItems = [];
-
   for (let i=0; i<dataList.length; i++) {
-    console.log(i, dataList[i])
-    listItems[i] = <Item comp={dataList[i]} />
+    listItems[i] = <Item key={dataList[i].id} comp={dataList[i]} />
   }
 
   return <div className={styles.itemList}>{listItems}</div>;
--- components/components/list_view.tsx
+++ components/components/list_view.tsx
@@ -1,6 +1,7 @@
 import { GenerateList } from "./generate_list";
 import { useEffect, useState } from "react";
 import { NEXT_PUBLIC_AUTH_TOKEN } from "../../constants";
+import { tmpdir } from "os";
 
 let stat = "uptime data unavailable!";
 
@@ -9,18 +10,6 @@ const ListView = function () {
   const [loaded, setLoaded] = useState(false);
   let idList=[];
   let pages: any;
-
-  const getComponentsFromPage = async (pageId : string) => {
-    const URL = `https://api.statuspage.io/v1/pages/${pageId}/components`;
-    const response = await fetch(URL, {
-      headers : {
-        "Content-Type": "application/json",
-        Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
-      }
-    })
-    const componentData = await response.json();
-    return componentData;
-  }
 
   const getPageID = async () => {
     const URL = "https://api.statuspage.io/v1/pages/"
@@ -32,33 +21,43 @@ const ListView = function () {
     })
     let xjson = await response.json();
     idList = xjson.map(data=>data.id);
-    return idList;
+    return idList; 
   }
 
   const getComponents = async () => {
-    pages = await getPageID();
-    let tmp = [];
-    pages.forEach(async (page: string) => {
-      let pageComponents = await getComponentsFromPage(page);
-      let i: number=0;
-      pageComponents.forEach((comp: any) => {
-        tmp[i++]=comp;
+    pages = await  getPageID();
+    let urls = pages.map(page => {
+      return `https://api.statuspage.io/v1/pages/${page}/components`;
+    })
+
+    let tmp=[];
+    let j=0;
+    await Promise.all(
+      urls.map(async (url: string) => fetch(url, {
+        headers : {
+          "Content-Type": "application/json",
+          Authorization: `OAuth ${NEXT_PUBLIC_AUTH_TOKEN ?? ""}`
+        }
       })
-    });
+      .then(res => res.json())
+      .then(data=>{
+        for(let i=0; i<data.length; i++)  {
+          tmp[j++]=(data[i]);
+        }
+      })
+    ))
+    setLoaded(true);
     setDataList(tmp);
   }
 
   useEffect(() => {
-    if(dataList.length>0) setLoaded(true);
-    else {
-      getComponents();
-    }
+    getComponents();
   },[]);
 
 
-  if(loaded)  return <GenerateList dataList={dataList} />;
-  else return <>No items</>;
+    if(loaded)  return <GenerateList dataList={dataList} />;
+    else return <>No items</>;
 
 };
 
-export default ListView;
+export default ListView; 
