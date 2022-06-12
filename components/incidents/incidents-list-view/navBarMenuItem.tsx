2bb88f00985dafdb6366fe05416001d5d2147310
samar-1601 <samar.pp.singh@gmail.com>
2022-06-12 11:51:43 +0530

    added comments for navbarMenuItem component

--- components/incidents/incidents-list-view/navBarMenuItem.tsx
+++ components/incidents/incidents-list-view/navBarMenuItem.tsx
@@ -11,19 +11,32 @@ import {
 
 // components
 import { Block } from "baseui/block";
+
+// props for NavBarMenuItems
 interface Props {
   currentPage: string;
   pageType: string;
   onClick: any;
 }
 
-function menuItemChanged(prevMenu: Props, nextMenu: Props) {
+/**
+ * Check function for React.memo on the NavBarMenuItem to reduce unnecesary renders for unchanged menu items
+ * @param prevMenu The previous selected menu item
+ * @param currentMenu The current selected menu item
+ * @returns true if the previous or the current item was untouched
+ */
+function menuItemChanged(prevMenu: Props, currentMenu: Props) {
   return (
     prevMenu.currentPage !== prevMenu.pageType &&
-    nextMenu.currentPage !== nextMenu.pageType
+    currentMenu.currentPage !== currentMenu.pageType
   );
 }
 
+/**
+ * Returns a NavbarMenu item based on the Props passed
+ * Memoized to reduced unnecessary renders
+ * now only the changed menu items(cuurent + previous menu items) are rendered and not the unchanegd ones
+ */
 export const NavBarMenuItem: React.FC<Props> = React.memo(
   ({ currentPage, pageType, onClick }) => {
     console.log("menu");
