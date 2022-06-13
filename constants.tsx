acffaee8f95fb87296f1deb38fc994e38760f9b4
patra-souvik <souvik.patra@sprinklr.com>
2022-06-14 02:20:08 +0530

    PAGE_ID added as constant

--- constants.tsx
+++ constants.tsx
@@ -1,6 +1,7 @@
 import { STATUSType } from "./variableTypes";
 
 export const NEXT_PUBLIC_AUTH_TOKEN = "c12226f5-9156-47cd-bf7c-a36379785a25";
+export const PAGE_ID = "7dwwybj29fy8";
 
 export const STATUS: STATUSType = {
   operational: 1,
