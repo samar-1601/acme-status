20b9a5c0de27b44d7788bbafad7e841630b20e32
patra-souvik <souvik.patra@sprinklr.com>
2022-06-14 20:47:47 +0530

    corrected constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -1,8 +1,6 @@
 import { STATUSType } from "./variableTypes";
 export const PAGE_ID = "7dwwybj29fy8";
 export const NEXT_PUBLIC_AUTH_TOKEN = "c12226f5-9156-47cd-bf7c-a36379785a25";
-export const PAGE_ID = "7dwwybj29fy8";
-
 
 /**
  * PageType
@@ -124,9 +122,9 @@ export const getIncidentStatusFromPost = (id: string) => {
 };
 
 export const ICON_URL = {
-  "partial_outage": "/paritial_outage.png",
-  "major_outage": "/major_outage.png",
-  "degraded_performance": "/degraded_performance.png",
-  "under_maintenance": "/under_maintenance.png",
-  "operational": "/operational2.webp"
-}
+  partial_outage: "/paritial_outage.png",
+  major_outage: "/major_outage.png",
+  degraded_performance: "/degraded_performance.png",
+  under_maintenance: "/under_maintenance.png",
+  operational: "/operational2.webp",
+};
