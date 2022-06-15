6b902c6070d87c84175dbd13ec96a20fe537b81b
samar-1601 <samar.pp.singh@gmail.com>
2022-06-15 18:37:57 +0530

    removed files for renaming

--- components/incidents/client-incident-view/about-this-page/BarsDataGenerator.tsx
+++
@@ -1,9 +0,0 @@
-export const GenerateBarsData = (days:number)=>{
-    let list:any[] = [];
-    for(let day = 0; day<days; day++)
-    {
-        const downTime = Math.floor(Math.random() * 70);
-        list.push(downTime);
-    }
-    return list;
-}
