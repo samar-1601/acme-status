53545ef58a170c4f74f06880062a21f1c3f165f9
samar-1601 <samar.pp.singh@gmail.com>
2022-06-17 13:01:46 +0530

    added title and title Icon

--- components/ClientsIndex.tsx
+++ components/ClientsIndex.tsx
@@ -1,17 +1,20 @@
 import { Block } from "baseui/block";
 import Head from "next/head";
+import { PAGE_NAME } from "../constants";
 import { ClientsHomePage } from "./incidents/client-incident-view/ClientHomePage";
 
 const ClientsIndex = () => {
   return (
     <>
       <Head>
+        <title>{PAGE_NAME} Status</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/titleIcon.png" />
       </Head>
       <Block>
         <ClientsHomePage />
