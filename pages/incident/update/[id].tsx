724be1d889550be7a4d8d99d330725b87b6008d4
patra-souvik <souvik.patra@sprinklr.com>
2022-06-21 23:22:43 +0530

    updated incident edit for tailwind css

--- pages/incident/update/[id].tsx
+++ pages/incident/update/[id].tsx
@@ -1,7 +1,6 @@
 import { Block } from "baseui/block";
 import { useRouter } from "next/router";
 import * as React from "react";
-import { pageStyle } from "../../../components/incidents/createIncidents/styles/BlockStyles";
 import UpdateIncident from "../../../components/incidents/updateIncidents/updateIncident";
 
 export default () => {
@@ -9,7 +8,7 @@ export default () => {
   console.log(router.asPath);
   let param = router.asPath.split("/")[3];
   return (
-    <Block {...pageStyle}>
+    <Block>
       <UpdateIncident incidentId={param} />
     </Block>
   );
