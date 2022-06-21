c68b6fa612af54cf88126925ea7c03d6d89d01fb
patra-souvik <souvik.patra@sprinklr.com>
2022-06-21 23:23:15 +0530

    added tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import App from "next/app";
-import '../styles/global.css'
+import "../styles/index.css";
 import { Provider as StyletronProvider } from "styletron-react";
 import { LightTheme, BaseProvider } from "baseui";
 import { styletron } from "../styletron";
 import { SessionProvider } from "next-auth/react";
+import { SnackbarProvider } from "baseui/snackbar";
 
 export default class MyApp extends App {
   render() {
@@ -13,7 +14,9 @@ export default class MyApp extends App {
       <SessionProvider session={pageProps.session}>
         <StyletronProvider value={styletron}>
           <BaseProvider theme={LightTheme}>
-            <Component {...pageProps} />
+            <SnackbarProvider placement="bottom">
+              <Component {...pageProps} />
+            </SnackbarProvider>
           </BaseProvider>
         </StyletronProvider>
       </SessionProvider>
