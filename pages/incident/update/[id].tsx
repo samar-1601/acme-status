5dd6cfabc4915314d3b97d12131bb2879c4be879
patra-souvik <souvik.patra@sprinklr.com>
2022-06-21 12:50:24 +0530

    changes style in updateIncident

--- pages/incident/update/[id].tsx
+++ pages/incident/update/[id].tsx
@@ -1,10 +1,16 @@
+import { Block } from "baseui/block";
 import { useRouter } from "next/router";
 import * as React from "react";
+import { pageStyle } from "../../../components/incidents/createIncidents/styles/BlockStyles";
 import UpdateIncident from "../../../components/incidents/updateIncidents/updateIncident";
 
 export default () => {
   const router = useRouter();
   console.log(router.asPath);
   let param = router.asPath.split("/")[3];
-  return <UpdateIncident incidentId={param} />;
+  return (
+    <Block {...pageStyle}>
+      <UpdateIncident incidentId={param} />
+    </Block>
+  );
 };
