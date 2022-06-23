26678f79770f66eaa3e025fc68c720fc5c543953
yash-burnwal <byash.iitk@gmail.com>
2022-06-23 21:49:48 +0530

    build fix ;_;

--- pages/component/new.tsx
+++ pages/component/new.tsx
@@ -37,7 +37,7 @@ const AddComponents: React.FC = () => {
       </PageSlot.Slot>
       <PageSlot.Slot name="rightContent">
         <Block className={styles.page}>
-          <ComponentCreationForm />
+          <ComponentCreationForm id="" />
         </Block>
       </PageSlot.Slot>
     </PageSlot>
