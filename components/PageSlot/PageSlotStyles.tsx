2c50d8977d65dbb18d98c1df3342a5938fe27cde
samar-1601 <samar.pp.singh@gmail.com>
2022-06-28 01:25:54 +0530

    sidebar fixes done

--- components/PageSlot/PageSlotStyles.tsx
+++ components/PageSlot/PageSlotStyles.tsx
@@ -16,7 +16,7 @@ export const leftNavBarStyles: BlockProps = {
   overrides: {
     Block: {
       style: {
-        minWidth: "360px",
+        minWidth: "310px",
         display: "flex",
         minHeight: "100vh",
         zIndex: 10,
@@ -45,14 +45,19 @@ export const constantPaneStyles: BlockProps = {
         minWidth: "48px",
         backgroundColor: "white",
         height: "100vh",
-        margin: "auto",
+        margin: "0 auto",
         padding: "0px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         boxShadow: " 0 1px 4px 0 rgba(10,10,20,0.15)",
-        zIndex: "10",
+        zIndex: 20,
         cursor: "pointer",
+        ":hover": {
+          boxShadow: " 0 2px 8px 0 rgba(0,0,0,0.20)",
+        },
+        top: 0,
+        position: "sticky",
       },
     },
   },
