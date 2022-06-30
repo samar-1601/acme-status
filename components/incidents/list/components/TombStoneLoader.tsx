b2c0fbae2452f49d8b8dbf3e13a7892852724841
samar-1601 <samar.pp.singh@gmail.com>
2022-06-30 16:57:03 +0530

    tombstone made responsive

--- components/incidents/list/components/TombStoneLoader.tsx
+++ components/incidents/list/components/TombStoneLoader.tsx
@@ -12,19 +12,19 @@ export const MyLoader = (props: any) => (
   <Block overrides={LIST_ITEM_LOADER_OVERRIDES}>
     <ContentLoader
       speed={4}
-      width={1084}
+      width="100%"
       height={120}
-      viewBox="0 0 1084 120"
+      // viewBox="0 0 1084 120"
       backgroundColor="#f3f3f3"
       foregroundColor="#b8b8b8"
       {...props}
     >
-      <rect x="2" y="86" rx="3" ry="3" width="1024" height="19" />
-      <rect x="1" y="4" rx="0" ry="0" width="241" height="20" />
-      <rect x="2" y="44" rx="0" ry="0" width="110" height="17" />
-      <rect x="124" y="45" rx="0" ry="0" width="110" height="15" />
-      <circle cx="971" cy="12" r="11" />
-      <circle cx="1011" cy="12" r="11" />
+      <rect x="2" y="72%" rx="3" ry="3" width="80%" height="19" />
+      <rect x="1" y="3.33%" rx="0" ry="0" width="22%" height="20" />
+      <rect x="2" y="37%" rx="0" ry="0" width="10%" height="17" />
+      <rect x="12%" y="37%" rx="0" ry="0" width="10%" height="15" />
+      <circle cx="93%" cy="12" r="11" />
+      <circle cx="97%" cy="12" r="11" />
     </ContentLoader>
   </Block>
 );
@@ -34,7 +34,7 @@ export const MyLoader = (props: any) => (
  */
 export const TombStoneLoader: React.FC = () => {
   return (
-    <Block marginTop="20px">
+    <Block marginTop="20px" maxWidth="100%">
       <MyLoader />
       <MyLoader />
       <MyLoader />
